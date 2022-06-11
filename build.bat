57852f117f6a3de5a8779ec698b43edd3cf80c18
Damien Lacroix <inforlac.lacroix@gmail.com>
2022-06-11 14:07:15 +0200

    minor fix

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 copy "dist\Games SysTray\games.json"
-pyinstaller --clean --noconfirm --uac-admin -i app.ico --add-data "style.qss;." --add-data "games.json;." -n "Games SysTray" app.pyw
+pyinstaller --clean --noconfirm --uac-admin -i app.ico --add-data "style.qss;." -n "Games SysTray" app.pyw
 del "dist\Games SysTray\opengl32sw.dll"
